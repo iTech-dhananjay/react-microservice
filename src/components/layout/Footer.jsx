f219a29fda5bc167da824d8de8fe1f718ae3b03f
Dhananjay <officialdhananjay76@gmail.com>
2024-11-27 23:15:19 +0530

    Add sidebar components

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
