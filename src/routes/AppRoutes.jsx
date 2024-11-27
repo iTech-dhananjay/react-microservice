ebb2196e92bde1975af1d0ee93f4d899b4879b27
Dhananjay <officialdhananjay76@gmail.com>
2024-11-27 12:35:53 +0530

    Correct file structure

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import HomePage from "../pages/HomePage";
-import LoginPage from "../pages/auth/LoginPage";
+// import HomePage from "../pages/HomePage";
+// import LoginPage from "../pages/auth/LoginPage";
 import RegisterPage from "../pages/auth/RegisterPage";
-import UserListPage from "../pages/users/UserListPage";
+// import UserListPage from "../pages/users/UserListPage";
 import ProtectedRoute from "./ProtectedRoute"; // Protecting routes like UserListPage
 
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
+            {/*<Route path="/" element={<HomePage />} />*/}
+            {/*<Route path="/login" element={<LoginPage />} />*/}
             <Route path="/register" element={<RegisterPage />} />
 
             {/* Protected routes */}
             <Route element={<ProtectedRoute />}>
-                <Route path="/users" element={<UserListPage />} />
+                {/*<Route path="/users" element={<UserListPage />} />*/}
             </Route>
         </Routes>
     );
